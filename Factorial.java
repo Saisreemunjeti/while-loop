da2b928cfacef6546a60498d15bbad9a958104f1
Saisreemunjeti <saisreemunjeti@gmail.com>
2025-05-19 20:08:45 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -1,5 +1,26 @@
 package com.loops;
 
 public class Factorial {
+   public int isFactorial (int num) {
+			if(num<0) {
+				System.out.println("Invalid Number");
+			}
+			if(num==0) {
+				System.out.println("Factorial of number is 1");
+			}
+			int fact=1;
+			int i = 1;
+			while (i <= num) {
+					fact*=i;
+					i++;
+				}
+			
+			return fact;
+		}
+		public static void main (String[] args) {
+			Factorial obj = new Factorial();
+			System.out.println(obj.isFactorial(7));
+			
+		}
 
-}
+	}
