905663f89a38f480883b2399174e7c328565769e
Saisreemunjeti <saisreemunjeti@gmail.com>
2025-05-19 20:09:34 +0530

    Update NeonNumber.java

--- NeonNumber.java
+++ NeonNumber.java
@@ -1,5 +1,30 @@
 package com.loops;
 
 public class NeonNumber {
+  public void isNeon(int num) {
+			if (num<0) {
+				System.out.println("Invalid Number");
+			}
+			int temp=num;
+			int square=num * num;
+			int sum =0;
+			while(square!=0) {
+				sum+= square%10;
+				square/=10;
+				
+			}
+			if (temp==sum) {
+				System.out.println(temp + " " + "is a Neon Number");
+			}
+			else {
+				System.out.println(temp + " " + "is not a Neon Number");
+				
+			}
+		}
+		public static void main (String [] args) {
+			NeonNumber obj = new NeonNumber ();
+			obj.isNeon(12);
+			
+		}
 
 }
